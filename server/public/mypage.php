ed91f255c1e95b0ace409eacb41120f149bb517c
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-10-27 20:40:39 +0900

    こめんと部分更新

--- server/public/mypage.php
+++ server/public/mypage.php
@@ -3,6 +3,15 @@ require_once __DIR__ . '/functions.php';
 
 $dbh =  connectDb();
 
+$sql = <<<EOM
+SELECT 
+    *
+FROM
+    sub_images
+WHERE
+    user_id=:id
+EOM;
+
 ?>
 
 <!DOCTYPE html>
@@ -12,20 +21,18 @@ $dbh =  connectDb();
 <body class="mypage">
     <?php include_once __DIR__ . '/_header.html' ?>
     <div class="photo-block">
-        <div class="user-photo">
-            <?php ?>
+        <img class="user-photo" src="<?= $sub_images['img'] ?>">
+        <a href=" photo.php" class="photo-plus-btn">Photo</a>
         </div>
-        <a href="photo.php" class="photo-plus-btn">Photo</a>
-    </div>
-    <a href="edit.php" class="edit-btn">編集</a>
-    <div class="plofile">
-        <ul>
-            <li>ユーザー名</li>
-            <li>居住地</li>
-            <li>タイプ</li>
-            <li>コメント</li>
-        </ul>
-    </div>
+        <a href="edit.php" class="edit-btn">編集</a>
+        <div class="plofile">
+            <ul>
+                <li>ユーザー名</li>
+                <li>居住地</li>
+                <li>タイプ</li>
+                <li>コメント</li>
+            </ul>
+        </div>
 </body>
 
 </html>
